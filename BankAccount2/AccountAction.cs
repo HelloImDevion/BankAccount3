f09f970185fe0861f61bbfd887ff6518976ecd06
HelloImDevion <devionfothergill@gmail.com>
2017-03-31 21:54:28 -0400

    first commit

---
+++ BankAccount2/AccountAction.cs
@@ -0,0 +1,24 @@
+﻿namespace BankAccount2
+{
+    public class AccountAction
+        {   //Fields
+            public decimal balance;
+            //Constructor
+            public AccountAction()
+            {
+                this.balance = balance;
+            }
+            //Methods
+            public virtual void DepositMoney(decimal balance, decimal deposit)
+            {
+                balance += deposit;
+            }
+
+            public virtual void WithdrawMoney(decimal balance, decimal withdraw)
+            {
+                withdraw -= balance;
+            }
+        }
+   }
+
+
